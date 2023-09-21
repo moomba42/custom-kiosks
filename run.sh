82fd03f5e1dedafb395af71607aa2e78506ede33
Aleksander Długosz <olekdlugi@gmail.com>
2023-09-21 14:52:19 +0200

    Added fonts to install and changed buttons script to remove pull-downs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install python3-pip omxplayer
+sudo apt-get install -y python3-pip omxplayer fonts-freefont-ttf
 sudo pip3 install adafruit-blinka
 
 wget -P ~/ https://github.com/Moomba42/rpi-audio-buttons/archive/refs/heads/main.zip
