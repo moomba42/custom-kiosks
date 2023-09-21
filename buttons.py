3b959d8c9d9c17af73b7fb6127fd677c287f6b22
Aleksander Długosz <olekdlugi@gmail.com>
2023-09-21 16:08:44 +0200

    Added player class to manage playback and stop other audio before playing new one

--- buttons.py
+++ buttons.py
@@ -2,8 +2,11 @@ import time
 import os
 import board
 import digitalio
+import logging
+from subprocess import Popen, PIPE, DEVNULL
 
-print("press a button!")
+logger = logging.getLogger(__name__)
+logger.log("Audio buttons started")
 
 button1 = digitalio.DigitalInOut(board.D2)
 button1.direction = digitalio.Direction.INPUT
@@ -37,41 +40,62 @@ button8 = digitalio.DigitalInOut(board.D26)
 button8.direction = digitalio.Direction.INPUT
 button8.pull = digitalio.Pull.UP
 
-while True:
+process = None
 
-    # omxplayer -o local <file>
-    # omxplayer -o hdmi <file>
-    # omxplayer -o both <file>
+class Player:
+    def __init__(self) -> None:
+        self.process = None
+    
+    def stop(self):
+        if self.process is not None:
+            if self.process.poll() is None:
+                self.process = None
+            else:
+                try:
+                    self.process.stdin.write('q') # send quit command
+                    self.process.terminate()
+                    self.process.wait()
+                except EnvironmentError as e:
+                    logger.error("Can't stop process")
+                else:
+                    self.process = None        
+    def play(self, path):
+        self.stop()
+        self.process = Popen(['omxplayer', path], stdin=PIPE, stdout=DEVNULL, close_fds=True, bufsize=0)
+
+player = Player()
+
+while True:
     if not button1.value:
         print("Button 1 pressed!")
-        os.system('omxplayer audio/gluszec.wav &')
+        player.play('audio/gluszec.wav')
 
     if not button2.value:
         print("Button 2 pressed!")
-        os.system('omxplayer audio/jelen.wav &')
+        player.play('audio/jelen.wav')
 
     if not button3.value:
         print("Button 3 pressed!")
-        os.system('omxplayer audio/nietoperze.wav &')
+        player.play('audio/nietoperze.wav')
 
     if not button4.value:
         print("Button 4 pressed!")
-        os.system('omxplayer audio/orzel.wav &')
+        player.play('audio/orzel.wav')
 
     if not button5.value:
         print("Button 5 pressed!")
-        os.system('omxplayer audio/pszczoly.wav &')
+        player.play('audio/pszczoly.wav')
 
     if not button6.value:
         print("Button 6 pressed!")
-        os.system('omxplayer audio/sowa.wav &')
+        player.play('audio/sowa.wav')
 
     if not button7.value:
         print("Button 7 pressed!")
-        os.system('omxplayer audio/wilk.wav &')
+        player.play('audio/wilk.wav')
 
     if not button8.value:
         print("Button 8 pressed!")
-        os.system('omxplayer audio/zaby.wav &')
+        player.play('audio/zaby.wav')
 
     time.sleep(.25)
