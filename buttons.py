8bdd19cf935dad246419cf39fdf220e2a0f49fd9
Aleksander Długosz <olekdlugi@gmail.com>
2023-09-21 15:27:41 +0200

    Added pull down entries

--- buttons.py
+++ buttons.py
@@ -7,27 +7,35 @@ print("press a button!")
 
 button1 = digitalio.DigitalInOut(board.D2)
 button1.direction = digitalio.Direction.INPUT
+button1.pull = digitalio.Pull.DOWN
 
 button2 = digitalio.DigitalInOut(board.D3)
 button2.direction = digitalio.Direction.INPUT
+button2.pull = digitalio.Pull.DOWN
 
 button3 = digitalio.DigitalInOut(board.D17)
 button3.direction = digitalio.Direction.INPUT
+button3.pull = digitalio.Pull.DOWN
 
 button4 = digitalio.DigitalInOut(board.D27)
 button4.direction = digitalio.Direction.INPUT
+button4.pull = digitalio.Pull.DOWN
 
 button5 = digitalio.DigitalInOut(board.D23)
 button5.direction = digitalio.Direction.INPUT
+button5.pull = digitalio.Pull.DOWN
 
 button6 = digitalio.DigitalInOut(board.D13)
 button6.direction = digitalio.Direction.INPUT
+button6.pull = digitalio.Pull.DOWN
 
 button7 = digitalio.DigitalInOut(board.D19)
 button7.direction = digitalio.Direction.INPUT
+button7.pull = digitalio.Pull.DOWN
 
 button8 = digitalio.DigitalInOut(board.D26)
 button8.direction = digitalio.Direction.INPUT
+button8.pull = digitalio.Pull.DOWN
 
 while True:
 
