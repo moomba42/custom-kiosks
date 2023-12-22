6922da533f84b205f45fb20a92df580319a4c391
Aleksander Długosz <olekdlugi@gmail.com>
2023-12-22 14:42:20 +0100

    Change script

--- rpi-photo-display/viewer.py
+++ rpi-photo-display/viewer.py
@@ -1,16 +1,15 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-master = tk.Tk()
-master.attributes('-fullscreen', True)
-master.configure(background='black')  # Set background color to black
+window = tk.Tk()
+window.attributes('-fullscreen', True)
+window.configure(background='black')  # Set background color to black
 
 image = Image.open('photo.jpg')
 photo = ImageTk.PhotoImage(image)
 
 # Create a label with the image and pack it to fill the entire screen
-label = tk.Label(master, image=photo)
-label.image = photo
+label = tk.Label(window, image=photo)
 label.pack(fill=tk.BOTH, expand=True)
 
-master.mainloop()
+window.mainloop()
